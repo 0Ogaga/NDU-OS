02799a5b547397a9bde6cc3b9e38f0c4634c81f9
Okwah Ogaga <ogagaokwah2021em@gmail.com>
2025-10-17 13:46:04 +0100

    Create lesson1.asm

---
+++ src/assembly_language_practice/lesson1.asm
@@ -0,0 +1,20 @@
+; SIMPLE DISPLAY
+
+; This is where we store data we are going to make use of in the program.
+SECTION .data
+    msg db 'Why always hello world!', 0xa
+
+; This is where we write the instructions to be executed by the CPU.
+SECTION .text
+
+global _start ; The 'global' makes our start label visible to the linker.
+_start:         ; This label is where the program starts executing.
+    mov     rax, 1          ; The syscall number for sys_write is 1
+    mov     rdi, 1          ; File descriptor 1 is stdout
+    mov     rsi, msg        ; Address of string to output
+    mov     rdx, 24         ; Number of bytes to write.
+    syscall                 ; Call the kernel so the CPU executes in kernel mode.
+    
+    
+    ; HINTS
+    ; A label is a named location in the code
